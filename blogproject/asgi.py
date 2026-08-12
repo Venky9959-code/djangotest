75f2abf1f85be7c42243cc05aacbe743c03d4d30
Banavathu Venkateswararao <venky995982@gmail.com>
2026-08-12 15:41:08 +0530

    Implemented blog functionality

--- blogproject/asgi.py
+++ blogproject/asgi.py
@@ -10,6 +10,5 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogproject.settings')
 application = get_asgi_application()
